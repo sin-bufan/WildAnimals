c860a1450335a536e08c34ecab5a08d932fef745
Bu Fan <bufan@hotmail.com>
2019-11-28 23:09:45 +0800

    fix the map

--- src/app/animal/map/map.component.ts
+++ src/app/animal/map/map.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-declare var L;
+import { Component ,AfterViewInit} from '@angular/core';
+import * as L from 'leaflet';
+//declare var L;
 @Component({
   selector: 'animal-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
   constructor() { }
   m:any;
-  ngOnInit() {
+  ngAfterViewInit() {
     this.initMap()
   }
   initMap(){
-    this.m = L.map('map').setView([110, 35 ], 10);
+    this.m = L.map('map').setView([110, 50 ], 11);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: 'Map tiles by osm',
-        minZoom: 3,
-        maxZoom: 10
+        minZoom: 1,
+        maxZoom: 11
     }).addTo(this.m);
-
+/* 
     var shpfile = new L.Shapefile('assets/data/congress.zip', {
 			onEachFeature: function(feature, layer) {
 				if (feature.properties) {
@@ -35,6 +36,6 @@ export class MapComponent implements OnInit {
 		shpfile.addTo(this.m);
 		shpfile.once("data:loaded", function() {
 			console.log("finished loaded shapefile");
-		});
+		}); */
   }
 }
